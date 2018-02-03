a6baaa4ec84d3ed0f64e0bd46fcba24ce29cb05e
jemand771 <jemand771@gmx.net>
2018-02-03 11:28:34 +0100

    ?
    ??

--- src/ajtest/Main.java
+++ src/ajtest/Main.java
@@ -1,9 +1,9 @@
 package ajtest;
 
 public class Main {
-
-	public static void main(String[] args) {
-
+	
+    public static void main(String[] args) {
+    	
 		System.out.println("Hello World! :)");
 		
 		System.out.println("what 1 nize dai");
